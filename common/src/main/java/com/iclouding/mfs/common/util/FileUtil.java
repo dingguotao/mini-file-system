005c78c51ff60fc1584ff5f565ce058d6905b487
iclouding <clouding.vip@qq.com>
2021-08-12 21:25:43 +0800

    修复文件名bug

--- common/src/main/java/com/iclouding/mfs/common/util/FileUtil.java
+++ common/src/main/java/com/iclouding/mfs/common/util/FileUtil.java
@@ -2,6 +2,8 @@ package com.iclouding.mfs.common.util;
 
 import com.google.common.io.Files;
 import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,10 +18,10 @@ import java.util.List;
  * @email: clouding.vip@qq.com
  */
 public class FileUtil {
+    private static final Logger logger = LoggerFactory.getLogger(FileUtil.class);
 
     public static String getEditLogFileName(long firstTxid, int size) {
-
-        return "editlog-" + firstTxid + "-" + (firstTxid + size) + ".log";
+        return "editlog-" + firstTxid + "-" + (firstTxid + size - 1) + ".log";
     }
 
     public static List<String> getFileData(String fileName) {
